983d9d04673f7fbd0c127670e60767a9c032ec38
MJ <davidrk@hey.com>
2021-12-27 23:39:32 +0000

    Update config to setup on Render

---
+++ bin/render-preview.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# This is a placeholder script to run some custom initialization for your preview environment after it is created but not on subsequent deploys.
+# See https://render.com/docs/preview-environments for more info.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,10 +3,10 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-    if ENV.fetch('IS_PULL_REQUEST') { false }
-      ENV['APPLICATION_HOST'] = ENV['RENDER_INSTANCE_ID'] + '.onrender.com'
-      ENV['ASSET_HOST'] = ENV['RENDER_INSTANCE_ID'] + '.onrender.com'
-    end
+  if ENV.fetch('IS_PULL_REQUEST') { false }
+    # If we're in a preview environment, set this to an internal email address so app emails are not sent to real users
+    ENV['EMAIL_RECIPIENTS'] = 'test@email.com'
+  end
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.asset_host  = ENV.fetch('ASSET_HOST', ENV.fetch('APPLICATION_HOST'))
+  config.asset_host = ENV.fetch('ASSET_HOST', ENV.fetch('RENDER_EXTERNAL_HOSTNAME'))
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: ENV.fetch('SMTP_ADDRESS'),
     authentication: :plain,
-    domain: ENV.fetch('SMTP_DOMAIN'),
+    domain: ENV.fetch('SMTP_DOMAIN', ENV.fetch('RENDER_EXTERNAL_HOSTNAME')),
     enable_starttls_auto: true,
     password: ENV.fetch('SMTP_PASSWORD'),
     port: 587,
@@ -107,13 +107,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.middleware.use Rack::CanonicalHost, ENV.fetch('APPLICATION_HOST')
+  config.middleware.use Rack::CanonicalHost, ENV.fetch('APPLICATION_HOST', ENV.fetch('RENDER_EXTERNAL_HOSTNAME'))
   config.middleware.use Rack::Deflater
   config.public_file_server.headers = {
     'Cache-Control' => 'public, max-age=31557600',
   }
-  config.action_mailer.default_url_options = { host: ENV.fetch('APPLICATION_HOST') }
-  config.action_mailer.asset_host = ENV.fetch('ASSET_HOST', ENV.fetch('APPLICATION_HOST'))
+  config.action_mailer.default_url_options = { host: ENV.fetch('APPLICATION_HOST', ENV.fetch('RENDER_EXTERNAL_HOSTNAME')) }
+  config.action_mailer.asset_host = ENV.fetch('ASSET_HOST', ENV.fetch('RENDER_EXTERNAL_HOSTNAME'))
   config.force_ssl = true
 
   # Inserts middleware to perform automatic connection switching.
