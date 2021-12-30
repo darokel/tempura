05a3b74207f4754300f93dd7a007d62b21a06a09
MJ <davidrk@hey.com>
2021-12-30 23:39:46 +0000

    Clean up ci setup

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,21 +6,6 @@ Capybara.register_driver :selenium_chrome do |app|
 end
 
 Capybara.register_driver :headless_selenium_chrome do |app|
-  ENV["CIRCLECI"] ? create_driver_for_remote_browser(app) : create_driver_for_local_browser(app)
-end
-
-def create_driver_for_remote_browser(app)
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :remote,
-    url: "http://localhost:4444/wd/hub",
-    capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-      'goog:chromeOptions' => { args: %w[headless no-sandbox window-size=1400,900] }
-    )
-  )
-end
-
-def create_driver_for_local_browser(app)
   config = IntegrationConfig.new
   config.enable_headless_mode!
   Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: config.options)
